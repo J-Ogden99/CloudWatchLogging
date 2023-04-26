e939816bc4b95f686b62768190a766467892568d
Josh Ogden <77122008+J-Ogden99@users.noreply.github.com>
2023-04-26 16:26:01 -0600

    Add example call

--- CloudWatch.py
+++ CloudWatch.py
@@ -1,20 +1,21 @@
 import boto3
 import logging
 import time
+import sys
 
 # Create a CloudWatch Logs client
 client = boto3.client('logs')
-
 # Set up logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-
 def log_request(**kwargs):
+    print(kwargs)
     # Construct the log message
     if 'version' not in kwargs or 'product_no' not in kwargs or 'format' not in kwargs:
         return
     message = f'{kwargs["version"]}_{kwargs["product_no"]}_{kwargs["format"]}'
+    print(message)
     if 'region_no' in kwargs:
         message += f'_{kwargs["region_no"]}'
     if 'link_no' in kwargs:
@@ -38,3 +39,8 @@ def log_request(**kwargs):
 
     # Print the response from CloudWatch
     print(response)
+
+#Example log
+request = 'dataservice.io/02/03/csv'
+params = request.split('/')[1:]
+log_request(version=params[0], product_no=params[1], format=params[2])
